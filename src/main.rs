b988debab9cc27bdf71ba63f45779b2a59f91c53
Will Becker <wibecker@pdx.edu>
2019-08-06 01:07:20 -0700

    Working on client. STILL BROKEN!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,48 @@
-mod roles;
-use roles::*;
+extern crate stdweb;
 
+use stdwen::traits::*;
+use stdweb::unstable::TryInto;
+use stdweb::web::{
+    HtmlElement,
+    document,
+    WebSocket,
+};
+
+use stdweb::web::event::{
+    KeyPressEvent,
+    SocketMessageEvent,
+};
+
+use stdweb::web::html_element::InputElement;
+
+/*
+fn name_promt(ws: WebSocket) {
+    let promt: InputElement = document()
+        .create_element("input")
+        .unwrap()
+        .try_into()
+        .unwrap();
+    promt.add_event_listener({move |event: KeyDownEvent| {
+            if event.key() == "Enter" {
+                ws.send_text(&promt.raw_value());
+            }
+        }
+}
+*/
 fn main() {
-    
+    stbweb::initialize();
+
+    /*
+    let ws = WebSocket::new("wss://localhost:3012").unwrap();
+
+    // https://github.com/koute/stdweb/blob/master/examples/echo/src/main.rs
+    ws.add_event_listener(move |event: SocketMessageEvent| {
+        match &event.data().into_text().unwrap() {
+            "givename" => name_promt(ws),
+            _ => panic("bad instruction from server"),
+        }
+    }
+    */
+
+    stdweb::event_loop();
 }
